bd899bb6295ea825ddd561d0a8041557969f1cbd
Alejandro Márquez <alejandromarquezmusic@gmail.com>
2020-02-19 06:49:40 +0100

    add authors array pass it as props

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import CommentDetail from "./CommentDatail";
+import faker from "faker";
 
 const App = () => {
+  const authors = [
+    faker.name.findName(),
+    faker.name.findName(),
+    faker.name.findName(),
+    faker.name.findName(),
+    faker.name.findName()
+  ];
+
   return (
     <div className="ui container comments">
-      <CommentDetail />
+      {authors.map(author => (
+        <CommentDetail author={author} key={author} />
+      ))}
     </div>
   );
 };
